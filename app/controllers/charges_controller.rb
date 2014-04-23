25c94a7f9355501222c92f9f659a59ca738383fa
Sam Meis <samanthakmeis@gmail.com>
2014-04-23 14:23:54 -0700

    added amount to pass to stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,7 +6,7 @@ class ChargesController < ApplicationController
 		@quantity = params[:quantity]
 		@amount = @quantity.to_f * 3500
 		@amount_formatted = @amount/100
-		params[:amount]=@amount
+		
 	end
 
 	def create
@@ -20,7 +20,7 @@ class ChargesController < ApplicationController
 
 	  charge = Stripe::Charge.create(
 	    :customer    => customer.id,
-	    :amount      => params[:amount],
+	    :amount      => 600,
 	    :description => params[:stripeDescription],
 	    :currency    => 'usd'
 	  )
