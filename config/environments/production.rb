e2a3ee3261802695449669405525c2d8795255b6
Sam Meis <samanthakmeis@gmail.com>
2014-04-23 13:33:37 -0700

    added lines to render static images upon deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Bloomslanding::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #to compile images for deployment 
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
